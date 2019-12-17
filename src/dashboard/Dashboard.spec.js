b6e27ce431ead5fc9f797c4e4b12c751a38d472d
Psage23 <pamelasage3@gmail.com>
2019-12-16 17:10:00 -0700

    MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from 'react'
+import Dashboard from './Dashboard';
+import { render } from '@testing-library/react';
+
+test("Render the Dashboard", () => {
+    render(<Dashboard />);
+});
+
+test("shows controls and display", () => {
+    const { getByText } = render(<Dashboard />);
+
+    const display = getByText(/open/i)
+    const controls = getByText(/close gate/i)
+
+    expect(display).toBeDefined();
+    expect(controls).toBeDefined();
+}) 
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react'
+import Display from './Display';
+import { render } from '@testing-library/react';
+
+test('Render the Display', () => {
+    render(<Display />)
+})
+
+
+
